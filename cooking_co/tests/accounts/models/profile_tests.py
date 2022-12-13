6c9f0b45fe4c78ffd45bfe129c8e8734c6f6f4c9
bogoivanov <102627890+bogoivanov@users.noreply.github.com>
2022-12-13 15:53:19 +0200

    some bugs fixed and send email

--- cooking_co/tests/accounts/models/profile_tests.py
+++ cooking_co/tests/accounts/models/profile_tests.py
@@ -1,6 +1,18 @@
 from unittest import TestCase
 
+from django.contrib.auth import get_user_model
 
+UserModel = get_user_model()
 class ProfileModelTests(TestCase):
     def test_profile_save__when_date_of_birth_correct__expect_correct_result(self):
-        pass
+        profile = UserModel(
+            email='bogoivanov@abv.bg',
+            password1='Cooking1234.',
+            password2='Cooking1234.',
+            date_of_birth='2000-11-11',
+        )
+
+        profile.full_clean()
+        profile.save()
+
+        self.assertIsNotNone(profile.pk)
